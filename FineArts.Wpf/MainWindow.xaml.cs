2fa338592bfa73e971477cc30ee27e79ad167ead
Francisco Cervantes Gaspar <franc_cg405@hotmail.com>
2022-12-06 20:27:15 -0600

    feature: Ui

--- FineArts.Wpf/MainWindow.xaml.cs
+++ FineArts.Wpf/MainWindow.xaml.cs
@@ -14,19 +14,30 @@ namespace FineArts.Wpf
         public MainWindow()
         {
             InitializeComponent();
-           
+
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+
             TeacherService teacherService = new();
             teacherList.DataContext = teacherService.GetTeachers();
+
         }
 
         private void saveChanges_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void teacherList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Teacher? selecteTeacher = teacherList.SelectedItem as Teacher;
+            TeacherService teacherService = new();
+            if (selecteTeacher != null)
+            {
+                studentList.DataContext = teacherService.GetStudentsByTeacherId(selecteTeacher.Id);
+            }
         }
     }
 }
